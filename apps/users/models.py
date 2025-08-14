4cedb37ef911fa97a049db7622ca654bf1cb6dd4
Juliano Jose Simao <fehribeiro074@gmail.com>
2025-08-13 21:19:39 -0300

    feat: Adição dos models User e Profile

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    full_name = models.CharField(max_length=150)  
+    date_birth = models.DateField(null=True, blank=True)  
+    email = models.EmailField(unique=True)  
+    password = models.CharField(max_length=128)  
+    phone = models.CharField(max_length=20, null=True, blank=True)  
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)  
+
+    def __str__(self):
+        return self.full_name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    nickname = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    photo = models.BinaryField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nickname
