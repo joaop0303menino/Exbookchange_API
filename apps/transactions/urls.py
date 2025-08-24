b4bdceb4f8d480f603be44b52bfba80a4e51bb0f
João Pedro Almeida Menino <joaopedroalmeidamenino@gmail.com>
2025-08-24 18:22:10 -0300

    Removida a URL de criação de anúncios do módulo de transactions

--- apps/transactions/urls.py
+++ apps/transactions/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import create_announce  # importa as views do app
+
 
 urlpatterns = [
-   path("announces/", create_announce, name="create_announce"),
+   
 ]
