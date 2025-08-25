f5df13b9ffe7f8124719957acba7dde9fd24bc88
Juliano Jose Simao <fehribeiro074@gmail.com>
2025-08-25 15:42:17 -0300

    feat(service): create announcement associating the book's author by full_name, creating it if necessary

--- apps/books/service/CreateAnnounceService.py
+++ apps/books/service/CreateAnnounceService.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from apps.books.models import Author
 from apps.users.models import User
 from ..models import Announces, ConservationStatus
 
@@ -8,8 +9,8 @@ def create_announce_service(validated_data, request_user):
         ConservationStatus, pk=validated_data["conservation_status_id"]
     )
 
-    author_id = validated_data.get("author_id")
-    author = get_object_or_404(User, pk=author_id) if author_id else request_user
+    author_full_name = validated_data.pop("author_full_name")
+    author, _ = Author.objects.get_or_create(full_name=author_full_name)
 
     announce = Announces.objects.create(
         title=validated_data["title"],
