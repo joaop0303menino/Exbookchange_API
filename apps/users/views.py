1cd5f5ef3a86372fd1ca1139c29cab9eb5c5d50b
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-08-26 15:07:43 -0300

    feat: add profile view

--- apps/users/views.py
+++ apps/users/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework.decorators import APIView
 from django.http import JsonResponse
 from rest_framework import status
-from apps.users.service.UserService import UserService
+from apps.users.services.UserService import UserService
 from apps.users.serializers import UserSerializer
+from apps.users.services.ProfileService import ProfileService
 
 class UserViews(APIView):
     def __init__(self):
@@ -16,3 +17,16 @@ class UserViews(APIView):
         serializer.save()
 
         return JsonResponse({"status": "success", "message": "User created successfully", "data": serializer.data}, status=status.HTTP_201_CREATED)
+
+class ProfileViews(APIView):
+    def __init__(self):
+        self.profile_service = ProfileService()
+
+    def post(self, request):
+        id_user = request.data.get("id_user")
+
+        if not id_user:
+            return JsonResponse({"status": "error", "message": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        response = self.profile_service.createProfile(**request.data)
+        return response
