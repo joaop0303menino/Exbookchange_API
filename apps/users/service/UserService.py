3ee198776a1ec91b0f2d87b69882fb40c80bb30f
jp_menino <joaopedroalmeidamenino@gmail.com>
2025-09-01 21:05:35 -0300

    feat(users): implementar criptografia da senha no cadastro de usuário
    - Atualizado o modelo User para herdar de AbstractUser e usar gerenciamento nativo de senhas
    - Implementado o uso do método set_password() para criptografar a senha antes de salvar
    - Ajustado o UserService para utilizar set_password garantindo a segurança da senha no cadastro

--- apps/users/service/UserService.py
+++ apps/users/service/UserService.py
@@ -1,26 +1,28 @@
-from django.http import JsonResponse
-from rest_framework import status
-from apps.users.models import User, Profile
+from django.http import JsonResponse 
+from rest_framework import status 
+from apps.users.models import User 
 
-
-class UserService:
-    def __init__(self):
-        self.user_model = User
-        self.profile_model = Profile    
-
-    def createUser(self, **data):
-        userExists = self.getUserByEmail(data["email"])
-
-        if userExists:
-            return JsonResponse({"status": "error", "message": "User already exists"}, status=status.HTTP_409_CONFLICT)
-
-        user =  self.user_model.objects.create(**data)
-        self.profile_model.objects.create(user=user, nickname=data.get("full_name"))
+class UserService: 
+    def __init__(self): 
+        self.user_model = User 
         
-        return user
-
-    def getUserByEmail(self, email):
-        return self.user_model.objects.filter(email=email).first()
-
-    def getUserById(self, id):
-        return self.user_model.objects.filter(id=id).first()
+    def createUser(self, validated_data): 
+        email = validated_data.get("email") 
+        userExists = self.getUserByEmail(email) 
+         
+        if userExists: 
+            return JsonResponse({"status": "error", "message": "User already exists"}, status=status.HTTP_409_CONFLICT) 
+        
+        user = self.user_model( 
+            full_name=validated_data.get("full_name"), 
+            date_birth=validated_data.get("date_birth"),
+            email=email, 
+            phone=validated_data.get("phone"), ) 
+        
+        user.set_password(validated_data['password']) 
+        user.save() 
+        
+        return user 
+    
+    def getUserByEmail(self, email): 
+        return self.user_model.objects.filter(email=email).first() 
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,5 @@
 from django.middleware.csrf import get_token
-from rest_framework.decorators import APIView
+from rest_framework.views import APIView
 from django.http import JsonResponse
 from rest_framework import status
 from apps.users.service.UserService import UserService
@@ -12,14 +12,19 @@ class UserViews(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        
-        user = self.user_service.createUser(**serializer.validated_data)
-        
-        if isinstance(user, JsonResponse):
-            return user
 
-        return JsonResponse({"status": "success", "message": "User created successfully","data": {"id": user.id, "full_name": user.full_name, "email": user.email}}, status=status.HTTP_201_CREATED)
-    
+        user = self.user_service.createUser(serializer.validated_data)
+
+        if isinstance(user, JsonResponse):
+            return user 
+
+        response_serializer = UserSerializer(user)
+        return JsonResponse({
+            "status": "success",
+            "message": "User created successfully",
+            "data": response_serializer.data
+        }, status=status.HTTP_201_CREATED)
+
 class TokenCSRFView(APIView):
     def get(self, request):
         token = get_token(request)
