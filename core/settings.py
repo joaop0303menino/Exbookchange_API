f40cda540ebee0256b81a700dc46b2f7eba45c1a
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-09-09 21:23:02 -0300

    chore: Config CORS

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'rest_framework',
+    'corsheaders',
 
     'apps.users',
     'apps.books',
@@ -27,6 +28,7 @@ INSTALLED_APPS = [
 ROOT_URLCONF = 'core.urls'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',          
     'django.middleware.common.CommonMiddleware',
@@ -40,7 +42,7 @@ AUTH_USER_MODEL = 'users.User'
 
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
-    default="http://localhost:3000",
+    default="http://localhost:5173",
     cast=Csv()
 )
 
