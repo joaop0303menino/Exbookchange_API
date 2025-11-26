7409e57da0d4267bc283b8e23f28eb764c1ede21
Bruno Henrique Ribeiro de Oliveira <163650715+BrunoOliveira0719@users.noreply.github.com>
2025-11-26 20:32:37 -0300

    feat: create route to update announce

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -3,4 +3,5 @@ from .views import AnnounceView
 
 urlpatterns = [
     path('announces/', AnnounceView.as_view(), name='announce-test'),
+    path('announces/<int:pk>/update', AnnounceView.as_view(), name='announce-test'),
 ]
